4b8275ea9ce0d3def06654cda7bc29d8376b91bc
Sai To Yeung <saitoyeung@gmail.com>
2015-04-07 08:13:15 -0400

    Download storage shed instead of scaffold data; Use redis

--- app/controllers/scaffoldings_controller.rb
+++ app/controllers/scaffoldings_controller.rb
@@ -9,12 +9,12 @@ class ScaffoldingsController < ApplicationController
 
     redis = Redis.new
 
-    some = current_scaffolding_data[0..200].map do |permit|
+    all_job_coordinates = current_scaffolding_data.map do |permit|
       job_coordinates = get_job_coordinates(redis, permit)
       [job_coordinates['latitude'], job_coordinates['longitude']]
     end
 
-    @response = JSON.generate(some)
+    @response = JSON.generate(all_job_coordinates)
     render :index
   end
 
@@ -39,8 +39,14 @@ class ScaffoldingsController < ApplicationController
 
   def cache_job_coordinates(redis, permit)
     job_number = permit['job__']
+    Geocoder.configure(
+      cache: Redis.new,
+      use_https: true,
+      google: {
+        api_key: ENV['SCAFFOLDING_APP_GOOGLE_API_KEY'],
+      }
+    )
     coordinates = Geocoder.coordinates("#{permit['house__'].strip} #{permit['street_name'].strip}, #{permit['zip_code'].strip}")
-
     job = {
       job_number: job_number,
       latitude: coordinates[0],
@@ -48,10 +54,20 @@ class ScaffoldingsController < ApplicationController
     }
 
     redis.hset("job_coordinates", job_number, job.to_json)
+    # unless coordinates.nil?
+
+      # job = {
+        # job_number: job_number,
+        # latitude: coordinates[0],
+        # longitude: coordinates[1]
+      # }
+
+      # redis.hset("job_coordinates", job_number, job.to_json)
+    # end
   end
 
   def get_current_open_nyc_data
-    json_response = HTTParty.get("https://data.cityofnewyork.us/resource/ipu4-2q9a.json?permit_subtype=SF&borough=MANHATTAN&$limit=10000&$select=job__,borough,house__,street_name,zip_code,expiration_date,job_start_date", headers: {"X-App-Token" => 'vkEMCryKaxhYrQyiXjlA3ZXqA'})
+    json_response = HTTParty.get("https://data.cityofnewyork.us/resource/ipu4-2q9a.json?permit_subtype=SH&borough=MANHATTAN&$limit=10000&$select=job__,borough,house__,street_name,zip_code,expiration_date,job_start_date", headers: {"X-App-Token" => 'vkEMCryKaxhYrQyiXjlA3ZXqA'})
     parsed_response = JSON.load(json_response.body)
     parsed_response.select! { |permit| Time.strptime(permit['expiration_date'], '%m/%d/%Y') > Time.now && Time.strptime(permit['job_start_date'], '%m/%d/%Y') < Time.now}
   end
